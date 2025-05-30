1705d7bf9590db00fa9bcbda9bb3fb995f24a529
tasnima831 <52164580+tasnima831@users.noreply.github.com>
2025-05-30 19:51:09 +0600

    Create word.py

---
+++ word.py
@@ -0,0 +1,29 @@
+easy_words = [
+    "Acorn", "Almond", "Apple", "Banana", "Beach", "Bubble", "Butter", "Cherry", "Cloud", "Comet",
+    "Dew", "Dinosaur", "Dog", "Dolphin", "Elephant", "Feather", "Flower", "Forest", "Giraffe", "Grass",
+    "Happy", "Ice", "Island", "Jasmine", "Jelly", "Kite", "Lemon", "Leaf", "Marshmallow", "Monkey",
+    "Mountain", "Nest", "Ocean", "Orange", "Pebble", "Penguin", "Pillow", "Pineapple", "Pumpkin", "Rainbow",
+    "River", "Seashell", "Shell", "Star", "Starfish", "Strawberry", "Sun", "Sunshine", "Tiger", "Tree",
+    "Tulip", "Turtle", "Umbrella", "Vanilla", "Waterfall", "Watermelon", "Wind", "Yellow", "Zen","Zebra"
+]
+medium_words = [
+    "Algorithm", "Binary", "Bytecode", "Compiler", "Database", "Debugging", "Efficiency", "Framework", "Function", "Git",
+    "Hash", "Iterator", "JavaScript", "Keyword", "Linux", "Machine", "Namespace", "Operator", "Parallel", "Queue",
+    "Recursion", "Session", "Template", "Unicode", "Version", "WebSocket", "Xdebug", "YAML", "Zero-day", "Analysis",
+    "Benchmark", "Caching", "Dependency", "Encapsulation", "IDE", "Jupyter", "Kubernetes", "Loop", "Middleware", "Protocol",
+    "Query", "Repository", "Stacks", "Transaction", "Asynchronous", "Candy", "Quilt", "Rainbow", "Sun", "Watermelon",
+    "Penguin", "Orange", "Banana", "Lemon", "Strawberry", "Vanilla", "Apple", "Peach", "Mango", "Kiwi", "Blueberry",
+    "Grapefruit", "Avocado", "Blackberry", "Raspberry", "Pineapple", "Cherry", "Coconut", "Cranberry", "Dragonfruit",
+    "Fig", "Guava", "Honeydew", "Kumquat", "Lime", "Lychee", "Nectarine", "Papaya", "Passionfruit","Persimmon"
+]
+hard_words = [
+    "Abecedarian", "Antidisestablishmentarianism", "Belligerent", "Cacophony", "Deteriorate", "Epiphany", "Flabbergasted", "Gobbledygook", "Higgledy-piggledy", "Ineffable",
+    "Juxtapose", "Kaleidoscope", "Labyrinthine", "Mellifluous", "Nebulous", "Obfuscate", "Pernicious", "Quixotic", "Resplendent", "Serendipity",
+    "Tumultuous", "Ubiquitous", "Vexatious", "Wunderkind", "Xenophobia", "Yuletide", "Zephyr", "Ambiguous", "Beleaguer", "Capitulate",
+    "Disparate", "Ebullient", "Farcical", "Garrulous", "Harangue", "Ineffable", "Juxtapose", "Kowtow", "Lugubrious", "Mellifluous",
+    "Nefarious", "Onomatopoeia", "Paroemiographer", "Quizzaciously", "Recrudescent", "Sphygmomanometer", "Tintinnabulation", "Ultracrepidarian", "Verisimilitude", "Whippersnapper",
+    "Xenoglossophobia", "Yttriferous", "Zoanthropy", "Encyclopedia", "Exaggeration", "Labyrinth", "Malicious", "Pseudopseudohypoparathyroidism", "Unimaginatively", "Xerophthalmia", "Dichlorodifluoromethane", "Hemidemisemiquaver",
+    "Sphygmomanometer", "Tintinnabulation", "Ultracrepidarian", "Verisimilitude", "Whippersnapper", "Xenoglossophobia", "Yttriferous", "Zoanthropy", "Antidisestablishmentarianism", "Disproportionableness",
+    "Unquestionableness", "Microspectrophotometrically", "Otorhinolaryngologist", "Paroemiographer", "Hypopotomonstrosesquippedaliophobia", "Zygomaticosphenoid", "Dichlorodifluoromethane", "Hemidemisemiquaver", "Incomprehensibilities", "Kinnikinnick",
+    "Lepidopterologist", "Macropodidae", "Nephelococcygia", "Obnubilated", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Quizzaciously", "Recrudescent", "Sphygmomanometer", "Tintinnabulation", "Ultracrepidarian"
+]
